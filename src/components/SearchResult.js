3ac76fd252d8f8f294e64a2fe51f84d4037bf8e5
porter3 <jakeporter310@gmail.com>
2020-04-26 14:10:58 -0400

    SearchResultTable now renders SearchResult components instead of the JSX directly

---
+++ src/components/SearchResult.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ListGroup, Button } from 'react-bootstrap';
+
+function SearchResult(props) {
+    const foodName = props.food.foodName;
+
+    return (
+        <ListGroup horizontal>
+            <ListGroup.Item>{foodName}</ListGroup.Item>
+            <ListGroup.Item>
+                <img
+                    src={props.food.photo}
+                    alt={foodName}
+                />
+            </ListGroup.Item>
+            <ListGroup.Item>
+                <Button
+                    onClick={props.onClick}
+                    type="button"
+                    variant="outline-dark">
+                    Analyze
+                </Button>
+            </ListGroup.Item>
+        </ListGroup>
+    );
+}
+
+
+
+export default SearchResult;
--- src/components/SearchResultTable.js
+++ src/components/SearchResultTable.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import './css/SearchResultTable.css'
+import SearchResult from './SearchResult';
 
 
-function SearchResultTable() {
-    return '';
+function SearchResultTable(props) {
+
+    const foodListItems = props.foodList.map((food, index) => {
+        return (
+            <SearchResult
+                key={food.foodName}
+                food={food}
+                onClick={() => props.onClick(food.foodName)}
+            />
+        );
+    });
+
+    return (
+        <ul>
+            {foodListItems}
+        </ul>
+    );
 }
 
 export default SearchResultTable;
