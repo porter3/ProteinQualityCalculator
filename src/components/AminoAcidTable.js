e9e75588f7e41408181a1c9352deb95d9124e087
porter3 <jakeporter310@gmail.com>
2020-04-26 17:30:51 -0400

    completed amino acid mapping functionality, moved function to separate file since it's lengthy

--- src/components/AminoAcidTable.js
+++ src/components/AminoAcidTable.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Table } from 'react-bootstrap';
+import WeightInput from './WeightInput';
+import mapAminoAcids from './helpers/mapAminoAcids';
 
 
-function AminoAcidTable() {
-    return '';
+
+function AminoAcidTable(props) {
+    
+    const aminoAcids = mapAminoAcids(props.foodDetails);
+    console.log(aminoAcids);
+
+    return (
+        <>
+            <WeightInput />
+            <Table>
+                <tbody>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </Table>
+        </>
+    );
 }
 
 export default AminoAcidTable;
---
+++ src/components/helpers/mapAminoAcids.js
@@ -0,0 +1,60 @@
+function mapAminoAcids(foodDetails) {
+    let grams, name;
+    return foodDetails.map(nutrient => {
+        grams = nutrient.value;
+        name = '';
+
+        switch (nutrient.attr_id) {
+            // tryptophan
+            case 501:
+                name = 'Tryptophan';
+                break;
+            case 502:
+                name ='Threonine';
+                break;
+            case 503:
+                name = 'Isoleucine';
+                break;
+            case 504:
+                name = 'Leucine';
+                break;
+            case 505:
+                name = 'Lysine';
+                break;
+            case 506:
+                name = 'Methionine';
+                break;
+            case 507:
+                name = 'Cysteine';
+                break;
+            case 508:
+                name = 'Phenylalanine';
+                break;
+            case 509:
+                name = 'Tyrosine';
+                break;
+            case 510:
+                name = 'Valine';
+                break;
+            // 511 is Arginine, leaving out on purpose
+            case 512:
+                name = 'Histidine';
+                break;
+            default:
+                break;
+        }
+
+        return ({
+            grams: grams,
+            name: name
+        });
+    })
+        .filter(nutrient => {
+            if (nutrient.name) {
+                return true;
+            }
+            return false;
+        });
+}
+
+export default mapAminoAcids;
