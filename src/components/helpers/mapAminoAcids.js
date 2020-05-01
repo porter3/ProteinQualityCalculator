dbc7720fdd360590968c18d373ccb84e804b8911
porter3 <jakeporter310@gmail.com>
2020-05-01 16:02:34 -0400

    fixed getRelativeAmount()

--- src/components/helpers/mapAminoAcids.js
+++ src/components/helpers/mapAminoAcids.js
@@ -11,53 +11,52 @@ function mapAminoAcids(foodDetails, totalProtein) {
             // tryptophan
             case 501:
                 name = 'Tryptophan';
-                relativeAmount = getRelativeAmount(grams, totalProtein, .0372);
+                relativeAmount = getRelativeAmount(grams, totalProtein, .007);
                 break;
             case 502:
                 name ='Threonine';
-                relativeAmount = getRelativeAmount(grams, totalProtein, .0745);
+                relativeAmount = getRelativeAmount(grams, totalProtein, .027);
                 break;
             case 503:
                 name = 'Isoleucine';
-                relativeAmount = getRelativeAmount(grams, totalProtein, .1064);
+                relativeAmount = getRelativeAmount(grams, totalProtein, .025);
                 break;
             case 504:
                 name = 'Leucine';
-                relativeAmount = getRelativeAmount(grams, totalProtein, .1489);
+                relativeAmount = getRelativeAmount(grams, totalProtein, .055);
                 break;
             case 505:
                 name = 'Lysine';
-                relativeAmount = getRelativeAmount(grams, totalProtein, .1277);
+                relativeAmount = getRelativeAmount(grams, totalProtein, .051);
                 break;
             /*
-            Upper end thresholds for cases 506-509 are based on estimates
-            since Methionine/Cysteine and Phenylalanine/Tyrosine
-            are ground together in USDA reommendations
+            Methionine/Cysteine and Phenylalanine/Tyrosine
+            are combined in USDA reommendations, split their percentages 50/50 here
             */
             case 506:
                 name = 'Methionine';
-                relativeAmount = getRelativeAmount(grams, totalProtein, .0691);
+                relativeAmount = getRelativeAmount(grams, totalProtein, .0125);
                 break;
             case 507:
                 name = 'Cysteine';
-                relativeAmount = getRelativeAmount(grams, totalProtein, .0691);
+                relativeAmount = getRelativeAmount(grams, totalProtein, .0125);
                 break;
             case 508:
                 name = 'Phenylalanine';
-                relativeAmount = getRelativeAmount(grams, totalProtein, .0745);
+                relativeAmount = getRelativeAmount(grams, totalProtein, .0235);
                 break;
             case 509:
                 name = 'Tyrosine';
-                relativeAmount = getRelativeAmount(grams, totalProtein, .0745);
+                relativeAmount = getRelativeAmount(grams, totalProtein, .0235);
                 break;
             case 510:
                 name = 'Valine';
-                relativeAmount = getRelativeAmount(grams, totalProtein, .1064);
+                relativeAmount = getRelativeAmount(grams, totalProtein, .032);
                 break;
             // 511 is Arginine, leaving out on purpose
             case 512:
                 name = 'Histidine';
-                relativeAmount = getRelativeAmount(grams, totalProtein, .1064);
+                relativeAmount = getRelativeAmount(grams, totalProtein, .018);
                 break;
             default:
                 break;
@@ -78,26 +77,24 @@ function mapAminoAcids(foodDetails, totalProtein) {
 }
 
 /*
-Takes the amount of the amino acid, total protein of the food,
-and what percentage of the total protein is in line with USDA reommendations.
-Returns whether that value is very low, low, middle, high, or very high
+Takes the amount of the amino acid, the total protein of the food,
+and the 'optimal' amount of the amino acid per gram (from USDA recommendations).
+Returns a relative value in the range of very low - very high.
 */
 
-// Does not return what's expected at the moment- all values are low/very low
-function getRelativeAmount(aminoQuantity, totalProtein, recommendationAvg) {
-    const upperThreshold = recommendationAvg * 2;
-    const percentageOfProtein = aminoQuantity / totalProtein;
+function getRelativeAmount(aminoQuantity, totalProtein, idealAmount) {
+    const aminoPerGram = aminoQuantity / totalProtein;
     let relativeAmount;
-    if (percentageOfProtein <= upperThreshold * .2) {
+    if (aminoPerGram <= idealAmount * .4) {
         relativeAmount = 'VERY LOW';
     }
-    else if (percentageOfProtein <= upperThreshold * .4) {
+    else if (aminoPerGram <= idealAmount * .8) {
         relativeAmount = 'LOW';
     }
-    else if (percentageOfProtein <= upperThreshold * .6) {
-        relativeAmount = 'MIDDLE';
+    else if (aminoPerGram <= idealAmount * 1.2) {
+        relativeAmount = 'MEDIUM';
     }
-    else if (percentageOfProtein <= upperThreshold * .8) {
+    else if (aminoPerGram <= idealAmount * 1.6) {
         relativeAmount = 'HIGH';
     }
     else {
